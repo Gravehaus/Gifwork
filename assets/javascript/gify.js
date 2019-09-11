1825290b0d613f69febbdac262baa2722ede310a
Julia graves <gravehaus@gmail.com>
2019-09-11 14:14:29 -0700

    commit

---
+++ assets/javascript/gify.js
@@ -0,0 +1,35 @@
+//searchGif("")
+    //
+    /*onlick="searchGif('Kingdom Hearts')"
+    onclick="searchGif('Final Fantasy')"
+    onclick="searchGif('Persona 4')"
+    onclick="searchGif('Persona 5')"
+    onclick="searchGif('Chrono Trigger')"
+    onclick="searchGif('Dark Souls')"
+    onclick="searchGif('Legend of Zelda')" 
+    
+    ----------------ACTUAL CODING------------------
+
+    TODO: SETUP GLOBAL CLICK HANDLER EVENTS
+        
+    TODO: 
+    TODO: CREATE SEARCHGIF FUNCTION
+        >FIVE GIFS PER SEARCH
+        >GET BUTTONS TO WORK 
+    TODO: SET IT UP AS STILL --> ONCE CLICKED BECOME ANIMATED
+    TODO: 
+
+    --------------------AESTHETICS------------------
+
+    TODO: PLACE GIFS IN CARDS
+    TODO:
+
+    -------------------MISC.--------------------
+    
+    TODO: FIGURE GITHUB. UGH....
+
+    */
+
+    $(document).on('click', 'myButton', function(){ //This is my click function
+
+    })
