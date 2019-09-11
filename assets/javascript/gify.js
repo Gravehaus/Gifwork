8f94237896eae31e92555b8275b3c87c74a7c629
Julia graves <gravehaus@gmail.com>
2019-09-11 16:16:46 -0700

    writing code now

--- assets/javascript/gify.js
+++ assets/javascript/gify.js
@@ -28,8 +28,49 @@
     
     TODO: FIGURE GITHUB. UGH....
 
-    */
-
-    $(document).on('click', 'myButton', function(){ //This is my click function
+$(document).on('click', 'myButton', function(){ //This is my click function
 
     })
+
+    */
+
+    var topics = ["Kingdom Hearts", "Final Fantasy", "Persona 4", "Persona 5", "Chrono Trigger", "Dark Souls", "Legend of Zelda"];
+
+    var numberOfGifs = 7;
+    var ratingCutOff = "R"; // ;D //
+
+    function makeButton(){
+        for ( var i = 0; i <topics.length; i++){
+            var newButton = $("<button");
+            newButton.addClass("btn");
+            newButton.addClass("jrpg-button");
+            newButton.text(topics[i]);
+            $("#button-container"). append(newButton);
+        }
+        $(".jrpg-button").unbind("click");
+        $(".jrpg-button").on("click", function(){
+            $("gif-img").unbind("click");
+            $("#gif-container").empty();
+            $("gif-container").removeClass("solid-border");
+            populateGIFContainer($(this).text());
+        });
+    }
+
+    function addButton(show){
+        if(topics.indexOf(show) === -1){
+            topics.push(show);
+            $("#button-container").empty();
+            renderButtons();
+        }
+    }
+
+    function populateGIFContainer(show){
+        $.ajax({
+            url:"" + show + "" +ratingCutOff + "&limit" +numberOfGifs, method: "GET"
+        }).then(function(response){
+            
+        })
+    }
+
+    
+
